4cc1535f4e8b84a15ce2beeb6968a8f26c70917a
tailyr debose <tailyrdebose2019@gmail.com>
2020-11-11 22:58:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_/chickenAndWaffles.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication_
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class chickenAndWaffles : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chicken_and_waffles)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+        }
+    }
